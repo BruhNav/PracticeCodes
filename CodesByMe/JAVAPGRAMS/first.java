4f516592c226e310217509cedef0e4279b6e4df3
Pranav Jaiswal <pranav20100@iitnr.edu.in>
2021-09-28 15:40:40 +0530

    did nothing

--- CodesByMe/JAVAPGRAMS/first.java
+++ CodesByMe/JAVAPGRAMS/first.java
@@ -1,20 +1,8 @@
-class Add
-{
-	public static void main(String[] args) {
-		int a,b;
-		a=10;
-		b=20;
-		int c=a+b;
-		int d=a-b;
-		int e =a*b;
-		int f=a/b;
-		int g=a%b;
+package CodesByMe.JAVAPGRAMS;
 
-		System.out.println("The valuse of c is"+c);
-		System.out.println("the vlsue of d is"+d);
-		System.out.println("the value of e is "+e);
-		System.out.println("the value of f is"+f);
-		System.out.println("the value of g is"+g);
-		System.out.println("The value of c,d,e,f,g are"+c+" "+d+" "+e+" "+f+" "+g );
-	}
+class Main{
+    public static void main(String[] args) {
+        System.out.print("s");
+        
+    }
 }
